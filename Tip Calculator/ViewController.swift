cb2dac54c4da6bf1aaa22671da578ecf533aefed
Sam Wander <samwander@fb.com>
2015-10-07 21:45:55 -0700

    Setup outlets

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
